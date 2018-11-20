eb2b60fef59b0aa6047005a0a21a9ed13e41bf19
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-20 02:02:49 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -45,7 +45,7 @@ class SongsController < ApplicationController
     flash[:notice] = "Song deleted."
     redirect_to songs_path
   end
-Song Clean,ARTIST CLEAN,Release Year,COMBINED,First?,Year?,PlayCount,F*G
+
   def upload
     CSV.foreach(params[:songs].path, headers: true) do |song|
       Customer.create(song_clean: song[0], artist_clean: song[1], release_year: song[2], combined: song[3], first: song[4], year: song[5], play_count: song[6], fg: song[7])
